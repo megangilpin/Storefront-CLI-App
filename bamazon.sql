2798d47b237444b804842865d682af18d74373e8
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-07-14 23:25:43 -0400

    checks quantity

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS bamazon;
+
 CREATE database bamazon;
 
 USE bamazon;
@@ -7,36 +9,36 @@ CREATE TABLE products(
   product_name VARCHAR(45) NOT NULL,
   department_name VARCHAR(45) NOT NULL,
   price DECIMAL (13, 4) NOT NULL,
-  stock_quanity INT NOT NULL,
+  stock_quantity INT NOT NULL,
   PRIMARY KEY (item_id)
 );
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("flour", "dry goods", 1.89, 3000);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("sugar", "dry goods", 2.08, 2000);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("batise dry shampoo", "beauty", 5.89, 1000);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("OxiClean Laundry Stain Remover Spray", "cleaning", 3.09, 3500);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("milk", "dairy", 2.99, 3500);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("kraft singles american cheese", "dairy", 2.99, 3000);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("pantene pro-v daily moisture renewal shampoo", "beauty", 4.97, 500);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("nature's bounty vitamin e-oil", "health", 4.83, 100);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("ball park hot dog buns", "bakery", 2.79, 600);
 
-INSERT INTO products (product_name, department_name, price, stock_quanity)
+INSERT INTO products (product_name, department_name, price, stock_quantity)
   VALUES("oscar mayer wieners", "meat", 2.50, 600);
--- bamazonCusomer.js
+++ bamazonCusomer.js
@@ -5,13 +5,13 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "Welcome468",
+  password: "",
   database: "bamazon"
 })
 
 connection.connect(function (err) {
   if (err) throw err;
-  displayProducts();
+  buyProduct();
 })
 
 // function that displays all the products in the store
@@ -19,12 +19,54 @@ function displayProducts() {
   var query = "SELECT item_id,product_name,price FROM products";
   connection.query(query,function (err, res) {
       if (err) throw err;
-      console.log(res)
       res.forEach(element => {
-        console.log (element.item_id, element.product_name, element.price);
+        console.log ("ID " + element.item_id + ": ", element.product_name, element.price);
       })
     }
   );
 }
 
+function buyProduct() {
+  inquirer.prompt([
+    {
+      type: "input",
+      name: "buy",
+      message: "What is the ID number of the product you would like to buy?"
+    },
+    {
+      type: "input",
+      name: "amount",
+      value: "number",
+      message: "How many would you like to buy?"
+    },
+  ]).then(function (answer) {
+  var orderAmount = answer.amount
+    connection.query(
+      "SELECT * FROM products WHERE ?",
+      {
+        item_id: answer.buy
+      }, 
+      function (err, res) {
+        if (err) throw err;
+        var stockAmount = res[0].stock_quantity;
+        var productName = res[0].product_name;
+        checkStock(stockAmount, orderAmount, productName);
+      }
+    )
+  });
+}
 
+// checks if the stock quantity is less than the amount. If their is enough stock the order is processed
+function checkStock(stockAmount, orderAmount, productName) {
+  if (stockAmount < orderAmount) {
+    console.log("Our stock on " + productName + " is too low, your order can not be processed. Sorry for the inconvenience.")
+  }
+  else{
+    processOrder();
+  }
+}
+
+// processes an order
+function processOrder() {
+  console.log("Order has been processed")
+};
