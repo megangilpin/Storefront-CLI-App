a49c813c5627eab7f5287997d62536a15114676d
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-07-18 23:40:40 -0400

    added video

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,17 +11,19 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  buyProduct();
+  displayProducts();
 })
 
 // function that displays all the products in the store
 function displayProducts() {
+  console.log("Welcome to Bamazon! We've got all the things you never knew you needed..." + "\n--------------")
   var query = "SELECT item_id,product_name,price FROM products";
   connection.query(query,function (err, res) {
       if (err) throw err;
       res.forEach(element => {
-        console.log ("ID " + element.item_id + ": ", element.product_name, element.price);
+        console.log("ID " + element.item_id + ": ", element.product_name + " $" + element.price + "\n--------------");
       })
+      buyProduct()
     }
   );
 }
@@ -61,29 +63,30 @@ function buyProduct() {
 // Checks if the stock quantity is less than the amount. If their is enough stock the order is processed
 function checkStock(stockAmount, orderAmount, productName, cost, id) {
   if (stockAmount < orderAmount) {
-    console.log("Our stock on " + productName + " is too low, your order can not be processed. Sorry for the inconvenience.")
+    console.log("--------------" + "\nOur stock on " + productName + " is too low, your order can not be processed. Sorry for the inconvenience.")
   }
   else{
     processOrder(stockAmount, orderAmount, productName, id, cost);
   }
+  connection.end()
 }
 
 // Processes an order and updates the mysql table with the new stock amount
 function processOrder(stockAmount, orderAmount, productName, id, cost) {
   var stockUpdate = parseFloat(stockAmount) - parseFloat(orderAmount);
-  var orderTotal = parseFloat(orderAmount) * parseFloat(cost);
-  connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [{
-      stock_quantity: stockUpdate 
-    },
-    {
-      item_id: id
-    }], 
-    function (err, res) {
-      if (err) throw err;
-      console.log("Your account will be charged $" + orderTotal + " for " + orderAmount + " units of " + productName)
-    }
+  var orderTotal = Math.round(parseFloat(orderAmount) * parseFloat(cost));
+    connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [{
+        stock_quantity: stockUpdate 
+      },
+      {
+        item_id: id
+      }], 
+      function (err, res) {
+        if (err) throw err;
+        console.log("--------------" + "\nYour account will be charged $" + orderTotal + " for " + orderAmount + " units of " + productName + ".")
+      },
     )
   };
   
